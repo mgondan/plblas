4016ccf797a0287ef128e4ba8b532815910b2857
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2023-12-19 09:08:03 +0100

    Add files via upload

---
+++ plblas.pl
@@ -0,0 +1,10 @@
+:- module(matrix, []).
+
+:- load_foreign_library(plblas).
+
+:- multifile user:portray/1.
+
+user:portray(Matrix) :- 
+    blob(Matrix, matrix), 
+    !,
+    matrix_portray(current_output, Matrix).
