76f84ff61d687c1272566d5ff049eac281f4f66f
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2023-12-27 18:29:36 +0100

    add framework for unit tests

--- prolog/plblas.pl
+++ prolog/plblas.pl
@@ -1,4 +1,4 @@
-:- module(plblas, []).
+:- module(plblas, [test_plblas/0]).
 
 :- load_foreign_library(foreign(plblas)).
 
@@ -8,3 +8,14 @@ user:portray(Matrix) :-
     blob(Matrix, matrix), 
     !,
     matrix_portray(current_output, Matrix).
+
+test_plblas :-
+  true.
+
+:- begin_tests(plblas).
+:- use_module(library(plblas)).
+
+test(plblas) :-
+  test_plblas.
+
+:- end_tests(plblas).
